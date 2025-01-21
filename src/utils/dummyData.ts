e5a11980140b018d05d95a17081780a3f469b6c8
Jack <jrgarces@uw.edu>
2025-01-20 22:17:34 -0800

    added dummy data

---
+++ src/utils/dummyData.ts
@@ -0,0 +1,18 @@
+export const cards = [
+    {
+      id: "A1 001",
+      name: "Bulbasaur",
+      image: "https://via.placeholder.com/150",
+      expansion: "Genetic Apex",
+      rarity: 1,
+      owner: "AshKetchum123",
+    },
+    {
+        id: "A1 002",
+        name: "Ivysaur",
+        image: "https://via.placeholder.com/150",
+        expansion: "Genetic Apex",
+        rarity: 2,
+        owner: "MistyFan222",
+    },
+  ];
